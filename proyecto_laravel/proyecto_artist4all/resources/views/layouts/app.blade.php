25f2d0954aa036e77b70628d2066b793abeb2e64
alecsung <asungyang@gmail.com>
2021-02-19 14:52:19 +0100

    Proyecto laravel con docker y tailwindcss, falta arreglar las migraciones

---
+++ proyecto_laravel/proyecto_artist4all/resources/views/layouts/app.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/css/app.css">
+    <title>Artist4all</title>
+</head>
+<body class="bg-gray-200">
+    <nav class="p-6 bg-white flex justify-between mb-4">
+        <ul class="flex items-center">
+            <li>
+                <a href="" class="p-3">Home</a>
+            </li>
+        </ul>
+        <ul class="flex items-center">
+            <li>
+                <a href="" class="p-3">Login</a>
+            </li>
+            <li>
+                <a href="" class="p-3">Register</a>
+            </li>
+            <li>
+                <a href="" class="p-3">Logout</a>
+            </li>
+        </ul>
+    </nav>
+    @yield('content')
+</body>
+</html>
