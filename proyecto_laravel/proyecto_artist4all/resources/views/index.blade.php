aebc604e2c7536691c5d4468ccad40aff503c96f
nuriadev <lahozbonaventuranuria@gmail.com>
2021-02-24 16:20:32 +0100

    eliminado laravel

--- proyecto_laravel/proyecto_artist4all/resources/views/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-    <div class="flex justify-center">
-        <div class="w-8/12 bg-white p-6 rounded-lg">
-            WELCOME TO MY LARAVEL PROJECT
-        </div>
-    </div>
-@endsection
