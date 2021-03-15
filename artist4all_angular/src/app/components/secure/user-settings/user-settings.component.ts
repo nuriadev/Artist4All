3a14c7a825d9e68eebd4b8d5db8361fd594abdfb
alecsung <asungyang@gmail.com>
2021-03-15 14:44:21 +0100

    version 15 de marzo

--- artist4all_angular/src/app/components/secure/user-settings/user-settings.component.ts
+++ artist4all_angular/src/app/components/secure/user-settings/user-settings.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { AuthenticationService } from 'src/app/core/services/authentication.service';
+import { SessionService } from 'src/app/core/services/session.service';
 import { UserService } from 'src/app/core/services/user.service';
 @Component({
   selector: 'app-user-settings',
@@ -9,9 +10,30 @@ import { UserService } from 'src/app/core/services/user.service';
 })
 export class UserSettingsComponent implements OnInit {
 
-  constructor(private _userService: UserService, private _router: Router) { }
+  constructor(
+    private _authenticationService: AuthenticationService,
+    private _sessionService: SessionService
+  ) { }
+
+  user = this._sessionService.getCurrentUser();
+  token = this._sessionService.getCurrentToken();
+  name:string;
+  surname1:string;
+  surname2:string;
+  email:string;
+  username:string;
+  password:string;
+  img:string;
 
   ngOnInit(): void {
+    this.name = this.user.name;
+    this.surname1 = this.user.surname1;
+    this.surname2 = this.user.surname2;
+    this.email = this.user.email;
+    this.username = this.user.username;
+    this.password = this.user.password;
+    this.img = this.user.img;
   }
+
 
 }
