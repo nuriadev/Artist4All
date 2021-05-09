a72601df6272ca968c8ccd3916e0a04f1431daf4
nuriadev <lahozbonaventuranuria@gmail.com>
2021-05-09 17:25:12 +0200

    style search results

--- artist4all_angular/src/app/components/secure/profile/index-profile/index-profile.component.ts
+++ artist4all_angular/src/app/components/secure/profile/index-profile/index-profile.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewChecked, Component, OnInit } from '@angular/core';
+import { AfterViewChecked, Component, ElementRef, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { SessionService } from 'src/app/core/services/session.service';
 import { UserService } from 'src/app/core/services/user.service';
@@ -6,7 +6,8 @@ import { NgxSpinnerService } from 'ngx-spinner';
 import { PublicationService } from 'src/app/core/services/publication.service';
 import { Publication } from 'src/app/core/models/publication';
 import { User } from 'src/app/core/models/user';
-
+import { ViewChild } from '@angular/core';
+import { Location } from '@angular/common';
 import {
   MatSnackBar,
   MatSnackBarHorizontalPosition,
@@ -21,13 +22,17 @@ import Swal from 'sweetalert2';
   providers: [UserService],
 })
 export class ProfileComponent implements OnInit, AfterViewChecked {
+  @ViewChild('imgPublication')
+  inputImgs: ElementRef;
+
   constructor(
     private _sessionService: SessionService,
     private _userService: UserService,
     private _publicationService: PublicationService,
     private _activeRoute: ActivatedRoute,
     private spinner: NgxSpinnerService,
-    private _snackBar: MatSnackBar
+    private _snackBar: MatSnackBar,
+    private _location: Location,
   ) {}
 
   publications: Array<Publication> = [];
@@ -53,9 +58,65 @@ export class ProfileComponent implements OnInit, AfterViewChecked {
   loaded: boolean;
   id_follow: number;
   status_follow: number;
+
+  images = [];
+  imgToUpload: FileList = null;
+  bodyPublication: string = '';
+  n_likes: number;
+  n_comments: number;
+  isEdited: number;
+  imgsReceived = [];
+
+  addImgPublication(imgPublication: FileList) {
+    this.imgsReceived = [];
+    this.images = [];
+    this.imgToUpload = imgPublication;
+    if (imgPublication && imgPublication[0]) {
+      var filesAmount = imgPublication.length;
+      for (let i = 0; i < filesAmount; i++) {
+        var reader = new FileReader();
+        reader.onload = (event:any) => {
+          this.images.push(event.target.result);
+        }
+        reader.readAsDataURL(imgPublication[i]);
+      }
+    }
+  }
+
+  removeSelectedImgs() {
+    this.images = [];
+    this.imgsReceived = [];
+    this.imgToUpload = null;
+    this.inputImgs.nativeElement.value = null;
+  }
+
+  miPublication;
+  id_publication: string = '';
+
   ngOnInit(): void {
     this.loaded = false;
     this._activeRoute.paramMap.subscribe((params) => {
+      this.id_publication = params.get('id_publication');
+        this._publicationService.getPublicationById(this.user.id, parseInt(this.id_publication)).subscribe(
+          (result) => {
+            this.miPublication = result;
+            this.id = this.miPublication.id;
+            this.user = this.miPublication.user;
+            this.bodyPublication = this.miPublication.bodyPublication;
+            this.n_likes = this.miPublication.n_likes;
+            this.n_comments = this.miPublication.n_comments;
+            this.isEdited = this.miPublication.isEdited;
+            if (result.imgsPublication != null) {
+              for (let i = 0; i < result.imgsPublication.length; i++) {
+                let file = result.imgsPublication[i];
+                this.imgsReceived.push(file.imgPublication);
+              }
+            }
+          },
+          (error) => {
+            console.log(error);
+          }
+        );
       this.spinner.show();
       setTimeout(() => {
         this.spinner.hide();
@@ -111,6 +172,26 @@ export class ProfileComponent implements OnInit, AfterViewChecked {
       }
     });
   }
+
+  editPublication() {
+    this.message = "Publicación editada.";
+    this._publicationService.edit(
+      new Publication(this.id, this.user, this.imgToUpload, this.bodyPublication, null, this.n_likes, this.n_comments, 0, 1)).subscribe(
+        (result) => {
+          this.openSnackBar(this.message);
+          this.redirectBack();
+        }, (error) => {
+          console.log(error);
+    });
+  }
+
+  redirectBack() {
+    this._location.back();
+  }
+
+  message: string;
+  horizontalPosition: MatSnackBarHorizontalPosition = 'right';
+  verticalPosition: MatSnackBarVerticalPosition = 'bottom';
 
   ngAfterViewChecked(): void {
     if (parseInt(this.id_user) != this.user.id) {
@@ -287,11 +368,7 @@ export class ProfileComponent implements OnInit, AfterViewChecked {
         console.log(error);
     });
   }
-
-  //#039be5
-  message: string;
-  horizontalPosition: MatSnackBarHorizontalPosition = 'right';
-  verticalPosition: MatSnackBarVerticalPosition = 'bottom';
+  
   openSnackBar(message: string) {
     this._snackBar.open(message, 'OK', { duration: 1000, horizontalPosition: this.horizontalPosition, verticalPosition: this.verticalPosition });
   }
