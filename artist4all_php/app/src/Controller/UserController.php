8e285b6e46862327686831886f5c055b15bea389
DAW2 <daw2@A22O22.local>
2021-05-17 16:27:19 +0200

    Login removed password pattern, home slightly changes, no password validation on login

--- artist4all_php/app/src/Controller/UserController.php
+++ artist4all_php/app/src/Controller/UserController.php
@@ -364,12 +364,6 @@ class UserController {
     } 
 
     $password = trim($data['password']);
-    $passwordPattern = '/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$#!%*?&])([A-Za-z\d$@#$!%*?&]|[^ ]){8,20}$/';
-    if(!filter_var($password, FILTER_VALIDATE_REGEXP,  array("options" => array("regexp" => $passwordPattern)))) {
-      $response = $response->withStatus(400, 'Wrong password format');
-      return $response;
-    }
-
     $user = \Artist4all\Model\User::getUserByEmail($email, 0);
     if (is_null($user)) {
       $response = $response->withStatus(400, 'Unvalid user');
